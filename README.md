dd225a8e051bb338335704fa87521b04b3983805
KhanLin <Khanzs@outlook.com>
2020-06-19 15:56:13 -0500

    pushing for the team

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Testing GIT
+
+
+- cd share-places
+- npm run android
+- npm run ios # you need to use macOS to build the iOS project - use the Expo app if you need to do iOS development without a Mac
+- npm run web
--- source.py
+++ source.py
@@ -2,6 +2,14 @@ import json
 import tweepy
 import re
 import csv
+from textblob import TextBlob
+import re
+
+
+consKey='m7D3STDCc6WMuZdIXDejSyZxi'
+consKeySec='suKdHr0ORtIYbqRxH3H1x6aNHcwxVlsgnBmtUbWjOEkxzPlyXV'
+acToken='1272604412666621957-oOVZTyJNKT0iGhSz04ptn6kKeA9i4F'
+acTokenSec='ELMqJ0M9RGYdb3AtvwI806beAisEuJHrh50VPk1IgaLQS'
 
 
 aKey= tweepy.OAuthHandler(consKey, consKeySec)
@@ -10,7 +18,19 @@ aKey.set_access_token(acToken, acTokenSec)
 api=tweepy.API(aKey)
 
 hashtag= input('Please enter the hashtag\n')
+hashtagList=hashtag.split()
+avg=0
+maxTweets=1000
+sentScore=0
 
-for tweet in tweepy.Cursor(api.search,q=hashtag+' -filter:retweets', lang="en", tweet_mode='extended').items(1000) : 
-    print(tweet.full_text.replace('\n', ' '))
-    print('\n')
+
+for x in hashtagList:
+    for tweet in tweepy.Cursor(api.search,q=x+' -filter:retweets', lang="en", tweet_mode='extended').items(maxTweets) : 
+        tweetSent=TextBlob(tweet.full_text)
+        sentScore+=tweetSent.sentiment.polarity
+        print(tweet.full_text.replace('\n', ' '))
+        print('\n')
+
+avg=sentScore/maxTweets
+print("The sentiment score for "+str(maxTweets)+" is "+str(round(avg*100,2)))
+ 
